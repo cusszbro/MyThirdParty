165cdab2fc84da4a1ec1fd98acc14f1535d86837
Luthfirrahman Dzulkarnain <luthfirluthfir@gmail.com>
2023-05-31 21:22:47 +0700

    update gradle
    update gradle and add margin layout on recyclerview

--- app/src/main/java/com/example/mythirdparty/data/remote/ApiConfig.kt
+++ app/src/main/java/com/example/mythirdparty/data/remote/ApiConfig.kt
@@ -1,5 +1,6 @@
 package com.example.mythirdparty.data.remote
 
+import com.example.mythirdparty.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -8,16 +9,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class ApiConfig {
     companion object {
-        private const val baseUrl = "https://api.github.com/"
-        private const val token = "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"
+        private const val baseUrl = BuildConfig.BASE_URL
+        private const val token = BuildConfig.KEY
 
         fun getApiService(): ApiService {
             val loggingInterceptor =
-//                if(BuildConfig.DEBUG) {
+                if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-//            } else {
-//                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-//            }
+            } else {
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+            }
             val authInterceptor = Interceptor { chain ->
                 val request = chain.request()
                 val headers = request.newBuilder()
